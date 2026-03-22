b5014a143dd213549021f833cab248896759adc3
gabriele-fontana <gabrielefontana.2001@gmail.com>
2026-03-22 22:29:25 +0100

    fix: update navbar brand name and enhance link accessibility with icons

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -9,16 +9,16 @@ export default function AppNavbar() {
         <>
                 <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">SHOP</a>
+                        <a className="navbar-brand" href="#">E-COMMERCE</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarID"
                             aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarID">
                             <div className="navbar-nav">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                                <a className="nav-link active" aria-current="page" href="/Products">Products</a>
-                                <a className="nav-link active" aria-current="page" href="/AboutUs">About Us</a>
+                            <a className="nav-link active" aria-current="page" href="/"><i className="bi bi-house-fill"></i> Home</a>
+                            <a className="nav-link active" aria-current="page" href="/Products"><i className="bi bi-cart-fill"></i> Products</a>
+                            <a className="nav-link active" aria-current="page" href="/AboutUs"><i className="bi bi-person-fill"></i> About Us</a>
 
                                 
                             </div>
