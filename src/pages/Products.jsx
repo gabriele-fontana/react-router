a5078b63380acdec16077094d73497c79af9ca43
gabriele-fontana <gabrielefontana.2001@gmail.com>
2026-03-22 21:59:31 +0100

    fetch and display products from API in Products component

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,45 @@
+import AppNavbar from "../components/AppNavbar";
+import { useState, useEffect, use } from "react";
 
-
-const apiUrl = 'https://fakestoreapi.com/'
+const apiUrl = 'https://fakestoreapi.com/products'
 
 export default function Products(){
+    
+    const [products, setProducts] = useState([])
+    
+    useEffect(()=>{
+        fetch(apiUrl)
+            .then(res => res.json())
+            .then(data => {
+
+                console.log(data);
+                setProducts(data)
+
+            })
+    },[])
 
 
-
-    return 
+    return  (
+        <>
+            <AppNavbar></AppNavbar>
+            <div className="container">
+                <h1>Products</h1>
+                <div className="row">
+                    {products.map(product => (
+                        <div key={product.id} className="col-md-4">
+                            <div className="card mb-4">
+                                <img src={product.image} className="card-img-top" alt={product.title} style={{ height: '200px', objectFit: 'contain' }} />
+                                <div className="card-body">
+                                    <h5 className="card-title text-truncate">{product.title}</h5>
+                                    <p className="card-text">${product.price}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
 
 
     
